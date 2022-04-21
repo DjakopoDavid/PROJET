6fb5d3feac13c34f6ee89698311a4d7fcf9a3f72
Youssouf <youssefbecker@gmail.com>
2022-04-21 03:16:58 +0200

    creation du formulaire ajouter un produit et de l'entité dans base de donnée 2

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,23 +3,41 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
 class ArticleController extends AbstractController
 {
-    #[Route('/article', name: 'article')]
+    #[Route('article/list', name: 'list')]
     public function index(): Response
     {
-        $articles = new Article();
-        $articles->setLibelle('mon article');
-        $articles->setPrix(250);
-        $articles->setQuantite(20);
-        $articles->setImage('img');
-        return $this->render('article/index.html.twig',[
-            'article' => $articles
-            ]
+        return $this->render('article/index.html.twig'
         );
+    }
+
+        #[Route('article/new', name: 'new')]
+    public function new(Request $request): Response
+
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleFormType::class, $article);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('list');
+        }
+        return $this->render('article/new.html.twig', [
+            "form" => $form->createView(),
+        ]);
     }
 }
--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,10 +14,10 @@ class ArticleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('libelle')
-            ->add('prix')
-            ->add('quantite')
-            ->add('image')
+            ->add('libelle', type: TextType::class)
+            ->add('prix', type: IntegerType::class)
+            ->add('quantite', type: IntegerType::class)
+            ->add('image', type: TextType::class)
         ;
     }
 
