110fbd9781f79f4886f0d89c7ec4d0c8d99ac03e
Youssouf <youssefbecker@gmail.com>
2022-04-26 03:23:28 +0200

    gestion du panier(suite)

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -9,16 +11,42 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
-    #[Route('/panier', name: 'panier')]
-    public function index(): Response
+    #[Route('/cart', name: 'cart')]
+    public function index(SessionInterface $sessionInterface, ArticleRepository $articleRepository)
     {
-        return $this->render('cart/cart.html.twig'
+        $panier = $sessionInterface->get("panier", []);
+        $dataPanier = [];
+        $total = 0;
+
+        foreach ($panier as $id => $quantite){
+            $article = $articleRepository->find($id);
+            $dataPanier[] = [
+                "article" => $article,
+                "quantite" => $quantite
+            ];
+            $total += $article->getPrix() * $quantite;
+
+        }
+        return $this->render('cart/cart.html.twig', compact("dataPanier", "total")
         );
     }
 
-    #[Route('/add/{id}', name: 'panier')]
-    public function add($id, SessionInterface $sessionInterface): Response
+    #[Route('/add/{id}', name: 'add')]
+    public function add(Article $article, SessionInterface $sessionInterface)
     {
+        $panier = $sessionInterface->get("panier", []);
+        $id = $article->getId();
+
+        if(!empty($panier[$id])){
+            $panier[$id]++;
+        }else{
+            $panier[$id] = 1;
+
+            $sessionInterface->set("panier", $panier);
+
+            return $this->redirectToRoute('cart');
+
+        }
 
 
     }
